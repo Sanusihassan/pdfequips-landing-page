b2fbb7c513ab1434e24c0b258b795005d0697cc9
Sanusi Hassan <sanusihassan122@gmail.com>
2023-07-12 02:51:40 +0000

    added styles & spinner to he editor

--- components/Markdown2PDF.tsx
+++ components/Markdown2PDF.tsx
@@ -1,30 +1,46 @@
-import dynamic from 'next/dynamic';
+// i want to show a spinner when this component is not loaded in particular the monacoeditor
+// i have react-bootstrap installed by the way
+
+import dynamic from "next/dynamic";
 import ReactMarkdown from "react-markdown";
 import { useState } from "react";
+import { Spinner } from "react-bootstrap";
 
-const MonacoEditor = dynamic(import('react-monaco-editor'), { ssr: false });
+const Loader = () => (
+  <div className="editor-loader">
+    <Spinner animation="grow" />
+    <p className="lead">please wait...</p>
+  </div>
+);
+
+const MonacoEditor = dynamic(() => import("react-monaco-editor"), {
+  ssr: false,
+  loading: () => <Loader />,
+});
 
 const Markdown2PDF: React.FC = () => {
   const [markdown, setMarkdown] = useState<string>("# hello world");
 
-  const handleEditorChange = (value) => {
+  const handleEditorChange = (value: string) => {
     setMarkdown(value);
-  }
+  };
 
   return (
     <div className="md-2pdf">
-      <MonacoEditor
-        language="markdown"
-        theme="vs-dark"
-        value={markdown}
-        onChange={handleEditorChange}
-        options={{ selectOnLineNumbers: true }}
-      />
+      <div className="editor">
+        <MonacoEditor
+          language="markdown"
+          theme="vs-dark"
+          value={markdown}
+          onChange={handleEditorChange}
+          options={{ selectOnLineNumbers: true }}
+        />
+      </div>
       <div className="react-markdown-container">
         <ReactMarkdown children={markdown} />
       </div>
     </div>
   );
-}
+};
 
 export default Markdown2PDF;
