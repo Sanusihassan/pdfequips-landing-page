00a29e0519d237b1d79d487d008d4a9edad7914c
planetcreative61 creativity <planetcreative61@gmail.com>
2023-07-16 12:46:53 +0000

    test push

--- components/EditPage.tsx
+++ components/EditPage.tsx
@@ -68,7 +68,7 @@ const EditPage = ({
           submitBtn.current.click();
         }
       }}
-      // disabled={state.errorMessage.length > 0}
+      disabled={state.errorMessage.length > 0}
     >
       <bdi>
         {
